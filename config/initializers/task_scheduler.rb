4b9ba32fad7c8ddd2e86e92e3a8fbf4271f624c5
Roberto Bartolome <roberto.bartolome.medina@gmail.com>
2013-11-07 14:06:04 +0100

    To Heroku

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,7 +6,7 @@ logger = Rails.logger
 logger.info('Starting the scheduler...')
 
 # Roberto 2013-11-04 modified to 15 minutes
-scheduler.every("5m") do
+scheduler.every("15m") do
   logger.info('Searching twitter...')
   ts = TwitterSearch.new
   ts.perform
