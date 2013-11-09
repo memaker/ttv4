2794126d0947d58992b4177c38bdcca2a53bfecf
Roberto Bartolome <roberto.bartolome.medina@gmail.com>
2013-11-09 18:51:07 +0100

    wip

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,12 +4,10 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.start_new
 logger = Rails.logger
 logger.info('Starting the scheduler...')
-puts 'Starting the scheduler...'
 
-# Roberto 2013-11-04 modified to 15 minutes
-scheduler.every("15m") do
+# Roberto 2013-11-04 updated the delay
+scheduler.every("5m") do
   logger.info('Searching twitter...')
-  puts 'Searching twitter...'
   ts = TwitterSearch.new
   ts.perform
 end
