95dc3c42fb3d9950c1736af113f69654768fc1fc
Roberto Bartolomé <roberto.bartolome.medina@gmail.com>
2014-03-03 22:47:31 +0100

    This is the first implementation of the ace_admin template and the GUI designed for the tt

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class DashboardController < ApplicationController
   def index
+
+    # this is for the tweets
+    @tweets = "hola"
+
+    # tweets per location map reduce and chart generation
+    @map = {
+        :center => {:latlng => [41.385116, 2.173423], :zoom => 12},
+        :markers => [{:latlng => [41.385116, 2.173423],
+                      :popup => "Hello!"
+                     }]
+    }
+
   end
 end
