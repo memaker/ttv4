bd8d136a0789e416585f8b71d9dd16ca2ebba4c0
Roberto Bartolomé <roberto.bartolome.medina@gmail.com>
2014-05-14 14:20:54 +0200

    Commit just before switching branch

--- app/workers/twitter_search.rb
+++ app/workers/twitter_search.rb
@@ -6,22 +6,20 @@ class TwitterSearch
     @gender_detector = SexMachine::Detector.new(:case_sensitive => false)
 
     # classifier
-    puts 'Creating the classifier for the Valence'
+    puts "Creating the classifier for the Valence"
     @classifier_bayes_valence = Classifier::Bayes.new '1', '2', '3', '4', '5'
     Anew.all.each do |anew|
       case anew.valmnall
-        when 1 .. 2
-          @classifier_bayes_valence.train_1 anew.sword
-        when 8 .. 9
-          @classifier_bayes_valence.train_5 anew.sword
-        when 2 .. 4
-          @classifier_bayes_valence.train_2 anew.sword
-        when 6 .. 8
-          @classifier_bayes_valence.train_4 anew.sword
-        when 4 .. 6
-          @classifier_bayes_valence.train_3 anew.sword
-        else
-          # type code here
+      when 1 .. 2
+        @classifier_bayes_valence.train_1 anew.sword
+      when 8 .. 9
+        @classifier_bayes_valence.train_5 anew.sword
+      when 2 .. 4
+        @classifier_bayes_valence.train_2 anew.sword
+      when 6 .. 8
+        @classifier_bayes_valence.train_4 anew.sword
+      when 4 .. 6
+        @classifier_bayes_valence.train_3 anew.sword
       end
     end
 =begin
@@ -41,22 +39,22 @@ class TwitterSearch
     # train it - notice split method used to tokenize text
     CorpusLead.all.each do |c|
       case c.leadorno
-        when 'l'
-          @nbayes.train(c.tweet.split(/\s+/), 'lead')
-        when 'n'
-          @nbayes.train(c.tweet.split(/\s+/), 'nolead')
-        else
-          # type code here
+      when 'l'
+        @nbayes.train( c.tweet.split(/\s+/), 'lead' )
+      when 'n'
+        @nbayes.train( c.tweet.split(/\s+/), 'nolead' )
       end
     end
 
   end
 
-  def perform
+  def perform()
     Term.all.asc(:searched_at).each do |term|
       options = {:result_type => "recent"}
       options = options.merge(term.last_id.nil? ? {} : {:since_id => term.last_id})
-      options = options.merge({:lang => "es"}) # tweets on Spanish only
+      # tweets on Spanish only
+      options = options.merge({:lang => "es"})
+      puts options
       search = Twitter.search(term.keywords, options)
 
       Rails.logger.info "Searching for terms #{term.description} with #{search.results.size.to_s} results."
@@ -72,16 +70,14 @@ class TwitterSearch
     begin
       tweet = Tweet.new
       # tweet = Tweet.new(status)
-
-      tweet.user = status.user.to_json
       tweet.name = status.user.name
-      tweet.screen_name = status.user.screen_name
+      tweet.username = status.user.name
       tweet.user_id = status.user.id
       tweet.lang = status.lang
       tweet.country_code = status.place.country_code if status.place
       # tweet.geo_enabled = status.user.geo_enabled
       # tweet.coordinates = status.coordinates
-      # tweet.location = status.user.location
+      tweet.location = status.user.location
       tweet.text = status.text
       tweet.hashtags = status.hashtags.map{|hashtag| hashtag.text}
       tweet.links = status.urls.map{ |url| url.url}
@@ -93,17 +89,17 @@ class TwitterSearch
       tweet.tweeted_at = status.created_at
 
       # gender calculation
-      tweet.gender = @gender_detector.get_gender(status.user.name.split(' ').first)
+      tweet.gender = @gender_detector.get_gender(status.user.name.split(" ").first)
 
       # valence calculation
       tweet.valence = @classifier_bayes_valence.classify status.text
 
+      # lead - nolead
       # tweet.lead = @classifier_bayes_lead.classify status.text
       result = @nbayes.classify(status.text.split(/\s+/))
       tweet.lead_data = result
-      tweet.lead = result.max_class # lead - nolead
+      tweet.lead = result.max_class
 
-      # associated term
       tweet.term = term
       tweet.save
     rescue Twitter::Error::TooManyRequests => error
@@ -111,7 +107,7 @@ class TwitterSearch
       sleep error.rate_limit.reset_in
       retry
     rescue Twitter::Error => error
-      puts error.backtrace
+      puts  error.backtrace
     end
   end
 
